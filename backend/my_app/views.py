40364813d970684f76bd3762c4fd15c08a6c47e5
Hayato Fujita <hayatoto0702@gmail.com>
2024-10-06 20:15:31 +0900

    viewの更新

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Article
 
@@ -8,8 +7,7 @@ from .models import Article
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     article_list = Article.objects.all()
-    template = loader.get_template("my_app/index.html")
     context = {
         "article_list": article_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "my_app/index.html", context)
