c3f32b459c3b1886efe15718e2122530fd95d63d
Hayato Fujita <hayatoto0702@gmail.com>
2024-10-06 20:06:06 +0900

    djangoでviewを表示できるようにした

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Article
+
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    article_list = Article.objects.all()
+    template = loader.get_template("my_app/index.html")
+    context = {
+        "article_list": article_list,
+    }
+    return HttpResponse(template.render(context, request))
