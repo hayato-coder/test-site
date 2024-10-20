53d08256a61e6d6c3d2d50563add13fa6b868221
Hayato Fujita <hayatoto0702@gmail.com>
2024-10-20 20:19:24 +0900

    databaseのデータがviewに表示されるようになった

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Article
-import rest_framework.response import Response
-    
+
 
 # Create your views here.
 class PostAll(APIView):
     def get(self, request):
         article = Article.objects.all()
         res_list = [
-            {
-                "id": a.id,
-                "title": a.title,
-                "content": a.content
-            }
-            for a in article
+            {"id": a.id, "title": a.title, "content": a.content} for a in article
         ]
         return Response(res_list)
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,20 +20,20 @@ export default function Home() {
       .then((data) => setData(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
-  const allPosts: Post[] = [
-    {
-      _id: '1',
-      slug: '/post/1',  // 記事の詳細ページへのパス
-      title: 'Sample Post Title',
-      description: 'This is a brief description of the sample post.',  // オプションの説明
-    },
-    {
-      _id: '2',
-      slug: '/post/2',  // 記事の詳細ページへのパス
-      title: 'Sample Post Title',
-      description: 'This is a brief description of the sample post.',  // オプションの説明
-    },
-  ];
+  // const allPosts: Post[] = [
+  //   {
+  //     _id: '1',
+  //     slug: '/post/1',  // 記事の詳細ページへのパス
+  //     title: 'Sample Post Title',
+  //     description: 'This is a brief description of the sample post.',  // オプションの説明
+  //   },
+  //   {
+  //     _id: '2',
+  //     slug: '/post/2',  // 記事の詳細ページへのパス
+  //     title: 'Sample Post Title',
+  //     description: 'This is a brief description of the sample post.',  // オプションの説明
+  //   },
+  // ];
   // allPostsはバックエンドにアクセスしてデータベースから取ってくる
   return (
     <div className="prose dark:prose-invert">
@@ -43,14 +43,14 @@ export default function Home() {
           <li key={item._id}>{item.title}</li>
         ))}
       </ul>
-      {allPosts.map((post) => (
+      {/* {allPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
-      ))}
+      ))} */}
     </div>
   )
 }
